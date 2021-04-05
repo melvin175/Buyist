bc19aa2ef42c32a9b23228185c1c38a36c1b12cf
apple <melvinfernando175@gmail.com>
2021-04-06 01:11:26 +0530

    Setting up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-400 text-4xl">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
