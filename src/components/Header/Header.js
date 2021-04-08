49293d1bdc9745cde24eca424101f590dcc47213
apple <melvinfernando175@gmail.com>
2021-04-09 01:59:13 +0530

    refactoring

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import React from 'react'
 
 const header = () => {
     return (
-        <div className="w-full mx-auto sticky inset-0 bg-gray-50  shadow h-14 flex p-9 text-3xl">
+        <div className="w-full mx-auto  bg-gray-50  shadow h-14 flex p-9 text-3xl">
            <div className="w-full flex items-center justify-between">
                 <a className="justify-center items-center flex">
                     <img src={Logo} style={{height:"12vh"}}/>
