63b3abb29caf0360a90e171d4e4162831477f491
apple <melvinfernando175@gmail.com>
2021-05-09 15:50:03 +0530

    Revert "Added hero section on home page"
    This reverts commit 42df9ef9e1a688b3d4ebd8654223c067fda66f71.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,26 +2,24 @@ import React from 'react'
 
 import { FcShipped } from 'react-icons/fc'
 import { RiShirtFill, RiErrorWarningFill } from 'react-icons/ri'
-import  Hero  from '../components/Hero/Hero'
 
 
 const Home = () => {
     return (
-        <div className="flex flex-col mx-auto px-10 ">
-            <span className="flex w-[95%] m-14 bg-gray-100 rounded-lg mx-auto p-3 tracking-widest px-20 space-x-24 items-center justify-center text-sm font-Bold">
+        <div className="flex flex-col mx-auto px-10">
+             <span className="flex w-[95%] m-14 bg-gray-100 rounded-lg mx-auto p-3 tracking-widest px-20 space-x-24 items-center justify-center text-sm font-Bold">
             <div className="flex">
-                <FcShipped className="mx-4 text-lg sm:invsible"/>Free shipping  
+                <FcShipped className="mx-4 text-lg"/>Free shipping  
             </div>
             <span> {"|"} </span>
             <div className="flex">
-            <RiShirtFill className="flex mx-4 text-lg text-black"/>Discover the freshest arrivals 
+            <RiShirtFill className="mx-4 text-lg text-black"/>Discover the freshest arrivals 
             </div>
             <span> {"|"} </span>
             <div className="flex">
             <RiErrorWarningFill className="mx-4 text-lg text-black"/>   Norms related co-vid followed
             </div>
             </span>
-            <Hero />
         </div>
     )
 }
